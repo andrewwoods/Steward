6b473fa42d60b888d39c714489cad88eeeb715b6
Andrew Woods <atwoods1@gmail.com>
2015-02-14 12:35:10 -0800

    Added vanilla JS code for manipulation of the class attribute.
    using examples from http://youmightnotneedjquery.com create code to
    * add a css class
    * verify a css class
    * toggle a css class
    * remove a css class
    
    Also created QUnit tests to exercise the operations

--- steward.js
+++ steward.js
@@ -1,12 +1,56 @@
-/*
-* Steward core class
-*/
+/**
+ * Steward core class
+ */
 var Steward = {
 	version: '0.1',
 	data: null,
 
 	load: function( data ){
 		Steward.data = data;
+	},
+
+	add_class: function ( el, className ) {
+
+		if ( el.classList ){
+			el.classList.add( className );
+		} else {
+			el.className += ' ' + className;
+		}
+	},
+
+	has_class: function( el, className ) {
+		if ( el.classList ) {
+			return el.classList.contains( className );
+		} else {
+			return new RegExp('(^| )' + className + '( |$)', 'gi').test( el.className );	
+		}
+	},
+
+	remove_class: function( el, className ){
+		if (el.classList) {
+			el.classList.remove(className);
+		 } else {
+			el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		}
+	},
+
+	toggle_class: function ( el, className ){
+
+		if ( el.classList ) {
+			el.classList.toggle( className );
+		} else {
+			var classes = el.className.split( ' ' );
+			var existingIndex = classes.indexOf( className );
+
+			if ( existingIndex >= 0 ){
+				classes.splice( existingIndex, 1 );
+			} else {
+				classes.push( className );
+			}
+
+			el.className = classes.join( ' ' );
+		}
 	}
 };
+
 
--- tests.js
+++ tests.js
@@ -17,5 +17,26 @@ QUnit.test( "load - check first item", function( assert ) {
 	assert.ok( Steward.data.menu[0].url == "/", "Passed!" );
 });
 
+QUnit.test( "Change class", function( assert ) {
+	var test_class = 'test-class';
+	var div = document.createElement( 'div' );
+
+
+	assert.ok( Steward.has_class( div, test_class )  === false, "Has class!" );
+
+	Steward.add_class( div, test_class );
+	assert.ok( Steward.has_class( div, test_class )  === true, "Class Added!" );
+
+	Steward.toggle_class( div, test_class );
+	assert.ok( Steward.has_class( div, test_class )  === false, "Class Toggled (Off)!" );
+
+	Steward.toggle_class( div, test_class );
+	assert.ok( Steward.has_class( div, test_class )  === true, "Class Toggled (On)!" );
+
+	Steward.remove_class( div, test_class );
+	assert.ok( Steward.has_class( div, test_class )  === false, "Class Removed!" );
+
+
+});
 
 
